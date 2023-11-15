dba5ecc7c801c8921098266b2ef105d4cc788312
Dragon <dragonofmercy@gmail.com>
2023-11-15 20:13:57 +0100

    Create new splited file

---
+++ Patches/AudioManagerPatches.cs
@@ -0,0 +1,16 @@
+﻿using CustomRadio.MonoBehaviours;
+using Game.Audio;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CustomRadio.Patches;
+
+[HarmonyPatch(typeof(AudioManager), "OnGameLoaded")]
+internal class AudioManagerOnGameLoadedPatch
+{
+    static void Postfix()
+    {
+        GameObject musicLoader = new GameObject("MusicLoader");
+        musicLoader.AddComponent<MusicLoader>();
+    }
+}
